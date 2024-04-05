bba601b2bffb7d1dbeedf030da4eecb90cdeb6d2
Utku Alcan <aalcan.utku@gmail.com>
2024-04-05 17:45:19 +0300

    Initial commit

---
+++ src/main/java/org/kurgu/moviemanagement/MovieManagementApplication.java
@@ -0,0 +1,13 @@
+package org.kurgu.moviemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kurgu/moviemanagement/MovieManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kurgu.moviemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
